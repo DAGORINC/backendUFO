1915e38d35e4c5eb5e5ccdf562bca082684ffa2e
dagor100 <damianxe@onet.pl>
2023-03-06 16:38:21 +0100

    normal http again

--- index.js
+++ index.js
@@ -6,12 +6,10 @@ const furnitureRouter = require('./App/routes/furnitureRouter/furnitureRouter');
 const imageSliderRouter = require('./App/routes/imageSlider/imageSliderRouter');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const fs = require('fs');
-const https = require('https');
 
 const port = process.env.PORT || 3000;
 
-if (!port) return console.log("Port is undefined: " + port);
+if(!port) return console.log("Port is undefined: "+port);
 
 const app = express();
 
@@ -22,7 +20,7 @@ require('./App/DataBase/mongoose');
 app.use(bodyParser.json());
 
 //Image static folder
-app.use(express.static(__dirname + './storage'))
+app.use('/storage', express.static('./storage'))
 
 //fix cors
 app.use(cors());
@@ -33,19 +31,7 @@ app.use('/api', collectionsRouter);
 app.use('/api', furnitureRouter);
 app.use('/api', imageSliderRouter);
 
-//ssl
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/da-gor.pl/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/da-gor.pl/cert.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/da-gor.pl/chain.pem', 'utf8');
-const credentials = {
-    key: privateKey,
-    cert: certificate,
-    ca: ca
-};
-
 //server
-const httpsServer = https.createServer(credentials, app);
-
-httpsServer.listen(port, () => {
+app.listen(port, function () {
     console.log(`Serwer działa na porcie: ${port}`);
 });
