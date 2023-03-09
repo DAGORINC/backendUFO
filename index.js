2d9d476096914e76ce222fc5889a7df166530d96
dagor100 <damianxe@onet.pl>
2023-03-09 01:38:41 +0100

    ssl trying

--- index.js
+++ index.js
@@ -33,24 +33,22 @@ app.use('/api', collectionsRouter);
 app.use('/api', furnitureRouter);
 app.use('/api', imageSliderRouter);
 
-//ssl
-// const privateKey = fs.readFileSync('/etc/letsencrypt/live/da-gor.pl/privkey.pem', 'utf8');
-// const certificate = fs.readFileSync('/etc/letsencrypt/live/da-gor.pl/cert.pem', 'utf8');
-// const ca = fs.readFileSync('/etc/letsencrypt/live/da-gor.pl/chain.pem', 'utf8');
-// const credentials = {
-//     key: privateKey,
-//     cert: certificate,
-//     ca: ca
-// };
+ssl
+const privateKey = fs.readFileSync('/etc/ssl/certs/pkey.key', 'utf8');
+const certificate = fs.readFileSync('/etc/ssl/certs/cert.crt', 'utf8');
+const credentials = {
+    key: privateKey,
+    cert: certificate
+};
 
-// //server
-// const httpsServer = https.createServer(credentials, app);
+//server
+const httpsServer = https.createServer(credentials, app);
 
-// httpsServer.listen(port, () => {
-//     console.log(`Serwer działa na porcie: ${port}`);
-// });
-
-
-app.listen(port, function () {
+httpsServer.listen(port, () => {
     console.log(`Serwer działa na porcie: ${port}`);
 });
+
+
+// app.listen(port, function () {
+//     console.log(`Serwer działa na porcie: ${port}`);
+// });
